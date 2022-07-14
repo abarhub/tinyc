a7d66a518261ca40316b97d625b45f5a5411df9e
abarhub <abarhub@gmail.com>
2022-07-14 09:30:39 +0200

    variabilisation du chemin vers le jeux de test

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # tinyc
+
+subset of c language
+
+- [x] lexer
+- [ ] parser (work in progress)
+- [ ] syntax analyser
+- [ ] interpretor (work in progress)
+
+syntax :
+- [ ] type : int, string
+- [ ] operator : +, -
+- [ ] function : printf
+- [ ] control structure : if, while
+
+
+Example :
+```C
+void main(){
+   int x=6;
+   int y;
+   y=8;
+}
+```
+
+
+
--- src/main.c
+++ src/main.c
@@ -1,34 +1,91 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <errno.h>
-#include<ctype.h>
-#include<malloc.h>
-#include<stdlib.h>
-#include<string.h>
-#include<assert.h>
-#include"main.h"
-#include"lexer.h"
-#include"parser.h"
-#include"run.h"
+#include <ctype.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <limits.h>
+#include "main.h"
+#include "lexer.h"
+#include "parser.h"
+#include "run.h"
 
+#if defined(_MSC_VER)
+#include <direct.h>
+#define getcwd _getcwd
+#elif defined(__GNUC__)
+#include <unistd.h>
+#endif
 
-void start(int argc, char* argv[]) {
+bool file_exists(char *filename)
+{
+	struct stat buffer;
+	return (stat(filename, &buffer) == 0);
+}
+
+char *findFile(char *filename)
+{
+	for (int j = 0; j < 5; j++)
+	{
+		char buf[2000] = "";
+		for (int i = 0; i < j; i++)
+		{
+			strcat(buf, "../");
+		}
+		strcat(buf, "exemples/");
+		strcat(buf, filename);
+		if (file_exists(buf))
+		{
+			printf("find file: %s",buf);
+			return filename;
+		}
+	}
+
+	return NULL;
+}
+
+void getCurrentDirectory(){
+	char cwd[500];
+   if (getcwd(cwd, sizeof(cwd)) != NULL) {
+       printf("Current working dir: %s\n", cwd);
+   } else {
+       perror("getcwd() error");
+       return 1;
+   }
+}
+
+void start(int argc, char *argv[])
+{
 
 	printf("Coucou\n");
 
-	Token* tokenList = NULL;
+	Token *tokenList = NULL;
 
-	char* file;
-	file= "..\\..\\..\\exemples\\test1.ci";
-	file = "..\\..\\..\\exemples\\test2.ci";
+	getCurrentDirectory();
+
+	char *filename;
+	char *file;
+	filename = "test1.ci";
+	filename = "test2.ci";
+
+	//filename = "../../exemples/test2.ci";
+
+	// file= "..\\..\\..\\exemples\\test1.ci";
+	// file = "..\\..\\..\\exemples\\test2.ci";
+	file = findFile(filename);
+	assert(file != NULL);
 
 	tokenList = lexer(file);
 
 	printToken(tokenList);
 
-	ASTFunction* ast = parse(tokenList);
+	ASTFunction *ast = parse(tokenList);
 
 	printAst(ast);
 
 	run(ast);
-
 }
